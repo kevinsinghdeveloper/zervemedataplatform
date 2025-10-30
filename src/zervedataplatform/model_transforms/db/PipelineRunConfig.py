90fec1036bdb107528955ed4fa5518d2fd41c729
kevin singh <kevinsingh.developer@gmail.com>
2025-10-29 21:06:38 -0400

    langchain embeddings connector configs

--- src/zervedataplatform/model_transforms/db/PipelineRunConfig.py
+++ src/zervedataplatform/model_transforms/db/PipelineRunConfig.py
@@ -7,6 +7,7 @@ from zervedataplatform.model_transforms.db.abstractions.PipelineRunConfigBase im
 @dataclass
 class PipelineRunConfig(PipelineRunConfigBase):
     ai_config: dict = None
+    ai_embeddings_config: Optional[dict] = None
     web_config: dict = None
     run_config: dict = None
     cloud_config: dict = None
